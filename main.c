61fd4349c41ac350a106b48a49ede828e992b360
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-04-14 01:45:50 +0000

    saving additions to main.c

--- main.c
+++ main.c
@@ -1,1 +1,35 @@
 #include "simple.h"
+// prompt user, loop till user input, cmd ends wit newline
+// prompt displays everytime after execution
+// cmd lines simple
+//cmd lines made of one word
+// if no executable found print error message
+// handle EOF
+//execve pass environ to it
+void shell_loop(char** env)
+{
+	char* input = NULL;
+	size_t input_size = 0;
+
+	char** args;
+
+	while (1)
+	{
+		printf("#cisfun$ ");
+		fflush(stdout);
+		if (getline(&input, &input_size, stdin) == -1)
+	{
+	      printf("\n");
+	      break;
+	}
+	printf("./shell: %s", input);
+	}
+free(input);
+}
+int main (int ac, char** av, char** env)
+{
+	(void)ac;
+	(void)av;
+
+	shell_loop(env);
+}
--- simple.h
+++ simple.h
@@ -2,4 +2,5 @@
 #define SIMPLE_H
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #endif
