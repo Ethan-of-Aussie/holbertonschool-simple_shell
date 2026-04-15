bbb8a435025d5645e511614202801c2e78505b4d
Sijin <sijinsingh222@gmail.com>
2026-04-15 21:29:06 +1000

    Add execve example in exec.c
    Implement execve example to execute '/bin/ls'. Fix argument passing and error handling.

---
+++ exec.c
@@ -0,0 +1,23 @@
+#include
+
+/**
+* main - execve example
+*
+* Return: 0
+*/
+
+int main(void)
+{
+  char *argv[] = {"/bin/ls", "-l", "/usr/", NULL};
+
+  printf("Before execve\n");
+
+  if (execve(argv[0], atgv, NULL == -1)
+  {
+    perror("Error:");
+  }
+
+  printf("After execve\n");
+
+  return (0);
+}
