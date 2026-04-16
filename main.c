9a6d593402c30bcb18dc792ae0c172e24bf8ae1d
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-04-16 11:08:38 +1000

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,4 @@
 #include "simple.h"
-// prompt user, loop till user input, cmd ends wit newline
-// prompt displays everytime after execution
-// cmd lines simple
-//cmd lines made of one word
-// if no executable found print error message
-// handle EOF
-//execve pass environ to it
 
 int main (int ac, char** av, char** env)
 {
