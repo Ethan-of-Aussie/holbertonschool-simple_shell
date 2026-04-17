eb498af5c3a1ed59894d4d26d28bd34f6a06cb9b
Sijin35 <sijinsingh222@gmail.com>
2026-04-17 10:52:43 +0000

    Changed error message, using fprintf instead of perror

--- execute.c
+++ execute.c
@@ -2,7 +2,14 @@
 
 void execute_command(char **args, char **env)
 {
-    pid_t pid = fork();
+    pid_t pid;
+    
+    if (!args || !args[0])
+    {
+	    return;
+    }
+
+    pid = fork();
 
     if (pid == -1)
     {
@@ -12,9 +19,11 @@ void execute_command(char **args, char **env)
 
     if (pid == 0)
     {
-        execve(args[0], args, env);
-        perror("./shell");
-        exit(1);
+        if (execve(args[0], args, env) == -1)
+	{
+		fprintf(stderr, "%s: command not found\n", args[0]);
+		exit(127);
+	}
     }
     else
     {
