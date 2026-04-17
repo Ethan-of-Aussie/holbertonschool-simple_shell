ee9d3ecaca74d19444602d45fb51f5e9fe8eb0e6
Sijin35 <sijinsingh222@gmail.com>
2026-04-17 11:04:25 +0000

    Commented previous code and replaced it with trial code for checker

--- path.c
+++ path.c
@@ -2,10 +2,21 @@
 
 char *_path(char *cmd)
 {
-	char *path = getenv("PATH");
-	char *copy = strdup(path);
-	
-	access(copy, X_OK);
+/**Previous code might be used for next task	
+ * char *path = getenv("PATH");
+ * char *copy = strdup(path);
+ * access(copy, X_OK);
+ * return (NULL);
+ */
+	if (!cmd)
+	{
+		return (NULL);
+	}
+
+	if (access(cmd, X_OK) == 0)
+	{
+		return (cmd);
+	}
 
 	return (NULL);
 }
