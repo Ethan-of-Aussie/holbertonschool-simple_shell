57d88269c2e952fef5a24bbddfc601ef21c5a15f
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-04-18 05:11:50 +0000

    Made progress with path, in simple.h addeda Max Line Size of 1024

--- path.c
+++ path.c
@@ -4,27 +4,29 @@ getenv()
 stat()
 access()
 strdup()
-
+use snprintf() not sprintf()
  */
 char *_path(char *cmd)
 {
 	char *path = getenv("PATH");
 	char *copy = strdup(path);
+	char *dir = strtok(copy, ":");
+	char full[MAX_LINE_SIZE];
+	struct stat buffer; /** from #include <sys/stat.h> */
 
 	if (!path)
 		return (NULL);
 	if (!copy)
 		return (NULL);
-
 	if (!cmd)
-	{
 		return (NULL);
-	}
-
 	if (access(cmd, X_OK) == 0)
-	{
-		return (cmd);
-	}
+		return (strdup(cmd));
 
+	while (dir)
+	  {
+	    
+	  }
+	free(copy);
 	return (NULL);
 }
--- simple.h
+++ simple.h
@@ -7,6 +7,7 @@
 #include <sys/wait.h>
 #include <sys/stat.h>
 #define MAX_INPUT 1024
+#define MAX_LINE_SIZE 1024
 void shell_loop(char** env);
 char** parse_input(char* input);
 void execute_command(char **args, char **env);
