b50c630d51b545a2bc938f1b9f7b1f272967baa7
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-04-20 01:32:26 +0000

    updated execute.c with pathing

--- execute.c
+++ execute.c
@@ -2,31 +2,35 @@
 
 void execute_command(char **args, char **env)
 {
-    pid_t pid;
-    
-    if (!args || !args[0])
-    {
-	    return;
-    }
+	pid_t pid;
+	char *pathing;
 
-    pid = fork();
-
-    if (pid == -1)
-    {
-        perror("fork");
-        return;
-    }
-
-    if (pid == 0)
-    {
-        if (execve(args[0], args, env) == -1)
+	if (!args || !args[0])
 	{
+		return;
+	}
+
+	pid = fork();
+
+	if (pid == -1)
+	{
+		perror("fork");
+		return;
+	}
+
+	if (pid == 0)
+	{
+		pathing = _path(args[0]);
+		if (pathing)
+		{
+			execve(pathing, args, env);
+			free(pathing);
+		}
 		fprintf(stderr, "%s: command not found\n", args[0]);
 		exit(127);
 	}
-    }
-    else
-    {
-        wait(NULL);
-    }
+	else
+	{
+		wait(NULL);
+	}
 }
