0b6f841c4b3da5f09f99972e9233f9dd4cfd2007
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-04-21 00:24:24 +0000

    changed 127 to 2 exit status

--- execute.c
+++ execute.c
@@ -11,29 +11,23 @@ int execute_command(char **args, char **env, int line)
 	int status;
 
 	if (!args || !args[0])
-	{
 		return (0);
-	}
 	if (_builtin(args, env))
 	  return (0);
 	pid = fork();
-
 	if (pid == -1)
 	{
 		perror("fork");
 		return (1);
 	}
-
 	if (pid == 0)
 	{
 		if (args[0][0] == '/' || args [0][0] == '.')
 		{
 			execve(args[0], args, env);
 			fprintf(stderr, "./hsh: %d: %s: not found\n", line, args[0]);
-			exit(127);
+			exit(2);
 		}
-		
-
 		pathing = _path(args[0]);
 		if (pathing)
 		{
@@ -44,23 +38,16 @@ int execute_command(char **args, char **env, int line)
 			execve(pathing, args, env);
 			free(pathing);
 		}
-
-		 if (errno == EACCES)
+		if (errno == EACCES)
         	{
-            		fprintf(stderr, "./hsh: %d: %s: Permission denied\n", line, args[0]);
-            		exit(126);
+			fprintf(stderr, "./hsh: %d: %s: Permission denied\n", line, args[0]);
+			exit(126);
         	}
-		
 		fprintf(stderr, "./hsh: %d: %s: not found\n", line,  args[0]);
-		exit(127);
+		exit(2);
 	}
-
 		waitpid(pid, &status, 0);
-
 		if (WIFEXITED(status))
-		{
 			return (WEXITSTATUS(status));
-		}
-
 		return (1);
 }
