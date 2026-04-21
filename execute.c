cce18159394c01d3ab83046008ba028364828392
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-04-21 11:22:02 +1000

    Update execute.c

--- execute.c
+++ execute.c
@@ -26,7 +26,7 @@ int execute_command(char **args, char **env, int line)
 		{
 			execve(args[0], args, env);
 			fprintf(stderr, "./hsh: %d: %s: not found\n", line, args[0]);
-			exit(2);
+			exit(127);
 		}
 		pathing = _path(args[0]);
 		if (pathing)
@@ -43,7 +43,7 @@ int execute_command(char **args, char **env, int line)
 			exit(126);
         	}
 		fprintf(stderr, "./hsh: %d: %s: not found\n", line,  args[0]);
-		exit(2);
+		exit(127);
 	}
 		waitpid(pid, &status, 0);
 		if (WIFEXITED(status))
