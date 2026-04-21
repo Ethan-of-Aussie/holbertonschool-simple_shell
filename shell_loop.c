4c0b700e3c1f4460ecb8c7a5034959a8739d06b2
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-04-21 04:11:17 +0000

    shell_loop changes

--- shell_loop.c
+++ shell_loop.c
@@ -27,6 +27,11 @@ int shell_loop(char **env)
 		args = parse_input(input);
 		if (!args || !args[0])
 		{
+		      if (!isatty(STDIN_FILENO))
+			{
+				free_tok(args);
+				break;
+			}
 			free_tok(args);
 			continue;
 		}
