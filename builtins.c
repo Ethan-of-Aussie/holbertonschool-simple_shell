9a2457bfd0b932b4718b8fac2cb3f1dba8c428fc
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-04-21 00:45:48 +0000

    pushing builtins

--- builtins.c
+++ builtins.c
@@ -9,10 +9,8 @@ int _builtin(char **args, char **env)
 {
 	int i;
 
-	/**
-	 * if (strcmp(args[0], "exit") == 0)
-	 * return(1);
-	 */
+	if (strcmp(args[0], "exit") == 0)
+	return(1);
 
 	if (strcmp(args[0], "env") == 0)
 	{
@@ -20,13 +18,6 @@ int _builtin(char **args, char **env)
 			printf("%s\n", env[i]);
 		return (1);
 	}
-
-	if (strcmp(args[0], "exit") == 0)
-	{
-		free_tok(args);
-		exit(0);
-	}
-	
 
 	return (0);
 }
